e0ec8397d2f329c11b3df3f1ddd891411d5db7a5
Sujall10 <94795609+Sujall10@users.noreply.github.com>
2024-07-04 12:47:06 +0530

    Update

--- Login.js
+++ Login.js
@@ -1,48 +1,53 @@
-const form = document.querySelector("form");
-eField = form.querySelector(".email"),
-    eInput = eField.querySelector("input"),
-    pField = form.querySelector(".password"),
-    pInput = pField.querySelector("input");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
 
-form.onsubmit = (e) => {
-    e.preventDefault();
+    if (form) {
+        const eField = form.querySelector(".email");
+        const eInput = eField.querySelector("input");
+        const pField = form.querySelector(".password");
+        const pInput = pField.querySelector("input");
 
-    (eInput.value == "") ? eField.classList.add("shake", "error"): checkEmail();
-    (pInput.value == "") ? pField.classList.add("shake", "error"): checkPass();
+        form.onsubmit = (e) => {
+            e.preventDefault();
 
-    setTimeout(() => {
-        eField.classList.remove("shake");
-        pField.classList.remove("shake");
-    }, 500);
+            (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+            (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
 
-    eInput.onkeyup = () => { checkEmail(); }
-    pInput.onkeyup = () => { checkPass(); }
+            setTimeout(() => {
+                eField.classList.remove("shake");
+                pField.classList.remove("shake");
+            }, 500);
+        };
 
-    function checkEmail() {
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        if (!eInput.value.match(pattern)) {
-            eField.classList.add("error");
-            eField.classList.remove("valid");
-            let errorTxt = eField.querySelector(".error-txt");
+        eInput.onkeyup = () => { checkEmail(); };
+        pInput.onkeyup = () => { checkPass(); };
 
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
-        } else {
-            eField.classList.remove("error");
-            eField.classList.add("valid");
+        function checkEmail() {
+            let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+            if (!eInput.value.match(pattern)) {
+                eField.classList.add("error");
+                eField.classList.remove("valid");
+                let errorTxt = eField.querySelector(".error-txt");
+
+                (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+            } else {
+                eField.classList.remove("error");
+                eField.classList.add("valid");
+            }
+        }
+
+        function checkPass() {
+            if (pInput.value == "") {
+                pField.classList.add("error");
+                pField.classList.remove("valid");
+            } else {
+                pField.classList.remove("error");
+                pField.classList.add("valid");
+            }
+        }
+
+        if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+            window.location.href = form.getAttribute("action");
         }
     }
-
-    function checkPass() {
-        if (pInput.value == "") {
-            pField.classList.add("error");
-            pField.classList.remove("valid");
-        } else {
-            pField.classList.remove("error");
-            pField.classList.add("valid");
-        }
-    }
-
-    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
-        window.location.href = form.getAttribute("action");
-    }
-}
+});
